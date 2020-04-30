cfebde46c6168425d1f965fb3b1f4085d23b4275
zloid <zloid-1@yandex.by>
2020-04-30 14:28:59 +0200

    added toFixed(2) for few selectors

--- src/features/addNewTransaction/selectors.js
+++ src/features/addNewTransaction/selectors.js
@@ -2,14 +2,13 @@ import React from 'react'
 //own
 import OneTransactionComponent from 'components/OneTransactionComponent'
 
-export const selectListOfAllTransaction = state => {
+export const selectListOfAllTransaction = (state) => {
   const {
     allTransactionList,
     euroToPlnExchangeRate,
   } = state.transactionsReducer
   return (
     allTransactionList
-      // .filter(obj => obj.visible === true)
       .filter(({ visible }) => visible === true)
       .map(({ idOfNewTransaction, nameOfTransaction, eurCount }) => (
         <OneTransactionComponent
@@ -17,16 +16,13 @@ export const selectListOfAllTransaction = state => {
           idOfNewTransaction={idOfNewTransaction}
           nameOfTransaction={nameOfTransaction}
           eurCount={eurCount}
-          plnCountOutput={eurCount * euroToPlnExchangeRate}
-
-          // plnCountOutput={setFloatingPoint(obj.eurCount * euroToPlnExchangeRate)}
-
+          plnCountOutput={Number((eurCount * euroToPlnExchangeRate).toFixed(2))}
         />
       ))
   )
 }
 
-export const selectSumOfAllTransactions = state => {
+export const selectSumOfAllTransactions = (state) => {
   const {
     allTransactionList,
     euroToPlnExchangeRate,
@@ -36,22 +32,20 @@ export const selectSumOfAllTransactions = state => {
   )
   const reducer = (accum, { eurCount }) => accum + eurCount
   const sumEur = filteredArray.reduce(reducer, 0)
-  const sumOfAllTransactionsPln = sumEur * euroToPlnExchangeRate
+  const sumOfAllTransactionsPln = Number(
+    (sumEur * euroToPlnExchangeRate).toFixed(2)
+  )
   return [sumEur, sumOfAllTransactionsPln]
 }
 
-export const selectMaxValTransaction = state => {
+export const selectMaxValTransaction = (state) => {
   const {
     allTransactionList,
     euroToPlnExchangeRate,
   } = state.transactionsReducer
-  // const allEurVal = allTransactionList.map(({ eurCount }) => eurCount)
-  // const maxFromAllEurVal = allEurVal.reduce((a, b) => Math.max(a, b))
-  // const maxFilteredFromAllEurVal = allTransactionList.filter(({ visible }) => visible === true).reduce(reducer, 0)
   const filteredFromAllEurVal = allTransactionList.filter(
     ({ visible }) => visible === true
   )
-
   if (filteredFromAllEurVal.length > 0) {
     const reducer = (accum, { eurCount }) => Math.max(accum, eurCount)
     const maxFilteredFromAllEurVal = filteredFromAllEurVal.reduce(reducer, 0)
@@ -60,12 +54,8 @@ export const selectMaxValTransaction = state => {
     )
     const maxTransactName = maxTransaction.nameOfTransaction
     const maxTransactEur = maxTransaction.eurCount
-    const maxTransactPln = maxTransactEur * euroToPlnExchangeRate
-    return [maxTransactName, maxTransactEur,  maxTransactPln]
+    const maxTransactPln = (maxTransactEur * euroToPlnExchangeRate).toFixed(2)
+    return [maxTransactName, maxTransactEur, maxTransactPln]
   }
-  // const maxTransactName = 'Lista transakcji jest pusta'
-  // const maxTransactEur = null
-  // const maxTransactPln = null
-  // return [maxTransactEur, maxTransactName, maxTransactPln]
   return ['Brak transakcji', 'Brak transakcji', 'Brak transakcji']
 }
