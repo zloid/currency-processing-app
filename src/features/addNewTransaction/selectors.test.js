02c5e07a2fc8e214b184f4c6bb7717c8e63ef7ce
zloid <zloid-1@yandex.by>
2020-05-13 19:12:25 +0200

    jsdoc implemented

---
+++ src/features/addNewTransaction/selectors.test.js
@@ -0,0 +1,10 @@
+import React from 'react'
+//own
+import OneTransactionComponent from 'features/addNewTransaction/OneTransactionComponent'
+import { selectListOfAllTransaction } from "./selectors";
+
+describe('selectListOfAllTransaction', () => {
+    it('run', () => {
+        // const 
+    })
+})
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,7 @@
 import { combineReducers } from 'redux'
-// import leftTextareaReducer from 'features/processDataFromTextareas/leftTextareaSlice'
+//own
 import transactionsReducer from 'features/addNewTransaction/transactionsSlice'
 
 export default combineReducers({
-    // leftTextareaReducer,
     transactionsReducer,
 })
-
-
-
