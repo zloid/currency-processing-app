f25a3f0680f40f21e1320c72ea2cae452f303e2e
zloid <zloid-1@yandex.by>
2020-05-16 17:03:29 +0200

    selectors and jsdocs refactoring

--- src/features/addNewTransaction/selectors.js
+++ src/features/addNewTransaction/selectors.js
@@ -2,9 +2,18 @@
 import React from 'react'
 //own
 import OneTransactionComponent from 'features/addNewTransaction/OneTransactionComponent'
- 
 /**
- * Selector for displaying active transactions, need JSX.Element OneTransactionComponent for work 
+ * 1.00 -> 1; 3.33333 -> 3.33;
+ * @function
+ * @name numberToFixedFloor
+ * @param {number} inNumber float
+ * @returns {number} floor, toFixed, float
+ */
+function numberToFixedFloor(inNumber) {
+  return Number(inNumber.toFixed(2))
+}
+/**
+ * Selector for displaying active transactions, need JSX.Element OneTransactionComponent for work
  * @function
  * @name selectListOfAllTransaction
  * @param {{}} state whole redux state
@@ -16,7 +25,7 @@ export const selectListOfAllTransaction = (state) => {
   /**
    * State object destructuring
    * @constant {[{visible: boolean, idOfNewTransaction: number, nameOfTransaction: string, eurCount: number}]} allTransactionList
-   */   
+   */
   const {
     allTransactionList,
     euroToPlnExchangeRate,
@@ -29,13 +38,26 @@ export const selectListOfAllTransaction = (state) => {
         key={idOfNewTransaction}
         idOfNewTransaction={idOfNewTransaction}
         nameOfTransaction={nameOfTransaction}
-        eurCount={eurCount}
-        plnCountOutput={Number((eurCount * euroToPlnExchangeRate).toFixed(2))}
+        eurCount={numberToFixedFloor(eurCount)}
+        // plnCountOutput={Number((eurCount * euroToPlnExchangeRate).toFixed(2))}
+        plnCountOutput={numberToFixedFloor(eurCount * euroToPlnExchangeRate)}
       />
     ))
 }
-
+/**
+ * Selector for counting amount of all transaction in eur and in pln
+ * @function
+ * @name selectSumOfAllTransactions
+ * @param {{}} state whole redux state
+ * @param {array.<object>} state.transactionsReducer.allTransactionList array of objects
+ * @param {number} state.transactionsReducer.euroToPlnExchangeRate float-number of actual rate
+ * @returns {array} [sumEur, sumOfAllTransactionsPln] -two float number in toFixed(2)
+ */
 export const selectSumOfAllTransactions = (state) => {
+  /**
+   * State object destructuring
+   * @constant {[{visible: boolean, eurCount: number}]} allTransactionList
+   */
   const {
     allTransactionList,
     euroToPlnExchangeRate,
@@ -44,14 +66,26 @@ export const selectSumOfAllTransactions = (state) => {
     ({ visible }) => visible === true
   )
   const reducer = (accum, { eurCount }) => accum + eurCount
-  const sumEur = filteredArray.reduce(reducer, 0)
-  const sumOfAllTransactionsPln = Number(
-    (sumEur * euroToPlnExchangeRate).toFixed(2)
+  const sumEur = numberToFixedFloor(filteredArray.reduce(reducer, 0))
+  const sumOfAllTransactionsPln = numberToFixedFloor(
+    sumEur * euroToPlnExchangeRate
   )
   return [sumEur, sumOfAllTransactionsPln]
 }
-
+/**
+ * Selector for get one biggest transaction from array of all transaction
+ * @function
+ * @name selectMaxValTransaction
+ * @param {{}} state whole redux state
+ * @param {array.<object>} state.transactionsReducer.allTransactionList array of objects
+ * @param {number} state.transactionsReducer.euroToPlnExchangeRate float-number of actual rate
+ * @returns {array} [maxTransactName, maxTransactEur, maxTransactPln] or ['Brak transakcji', 'Brak transakcji', 'Brak transakcji']
+ */
 export const selectMaxValTransaction = (state) => {
+  /**
+   * State object destructuring
+   * @constant {[{visible: boolean, eurCount: number}]} allTransactionList
+   */
   const {
     allTransactionList,
     euroToPlnExchangeRate,
@@ -66,8 +100,10 @@ export const selectMaxValTransaction = (state) => {
       ({ eurCount }) => eurCount === maxFilteredFromAllEurVal
     )
     const maxTransactName = maxTransaction.nameOfTransaction
-    const maxTransactEur = maxTransaction.eurCount
-    const maxTransactPln = (maxTransactEur * euroToPlnExchangeRate).toFixed(2)
+    const maxTransactEur = numberToFixedFloor(maxTransaction.eurCount)
+    const maxTransactPln = numberToFixedFloor(
+      maxTransactEur * euroToPlnExchangeRate
+    )
     return [maxTransactName, maxTransactEur, maxTransactPln]
   }
   return ['Brak transakcji', 'Brak transakcji', 'Brak transakcji']
