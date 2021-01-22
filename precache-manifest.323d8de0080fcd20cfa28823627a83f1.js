a21e6f263dbc6684454a5dfe8252f91c98d4fc00
zloid <zloid-1@yandex.by>
2021-01-22 15:34:53 +0100

    Updates

---
+++ precache-manifest.323d8de0080fcd20cfa28823627a83f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e92329695230d7db1c3de88b44c89e7",
+    "url": "/currency-processing-app/index.html"
+  },
+  {
+    "revision": "77207cb7fd082b407e03",
+    "url": "/currency-processing-app/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "3bf45bec267939af1cca",
+    "url": "/currency-processing-app/static/css/main.4ee0c25a.chunk.css"
+  },
+  {
+    "revision": "77207cb7fd082b407e03",
+    "url": "/currency-processing-app/static/js/2.2167a1b6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/currency-processing-app/static/js/2.2167a1b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bf45bec267939af1cca",
+    "url": "/currency-processing-app/static/js/main.24715856.chunk.js"
+  },
+  {
+    "revision": "8c03693609ebe7605016",
+    "url": "/currency-processing-app/static/js/runtime-main.111d2895.js"
+  }
+]);
